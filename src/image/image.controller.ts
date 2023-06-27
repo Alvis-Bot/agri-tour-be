c47e7c7c7a50e5fa58bdf8435b77303b1ad1d885
Võ Việt <vietvd@thomi.com.vn>
2023-06-27 23:05:56 +0700

    update api

---
+++ src/image/image.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Inject, Query, Res } from "@nestjs/common";
+import { Router } from "../common/enum/router";
+import { IImageService } from "./service/image";
+import { Service } from "../common/enum/service";
+import { Description } from "../common/decorator/description.decorator";
+import { ApiTags } from "@nestjs/swagger";
+
+@Controller(Router.IMAGE)
+@ApiTags('Image APIs')
+export class ImageController {
+
+  constructor (@Inject(Service.IMAGE_SERVICE) private readonly imageService: IImageService) {}
+
+  @Get()
+  @Description("Lấy ảnh theo tên ảnh")
+  async getImgById(@Res() response ,@Query('name') name: string){
+    response.sendFile(name, { root: 'uploads' });
+  }
+
+}
