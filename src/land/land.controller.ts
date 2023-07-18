59e7fa80f8fc04b2a0e6b055f41490d9f737d62a
hoanghaiduong <hoanghaiduong1711@gmail.com>
2023-07-18 10:10:55 +0700

    ssss

--- src/land/land.controller.ts
+++ src/land/land.controller.ts
@@ -72,22 +72,23 @@ export class LandController {
       throw new BadRequestException('Images file is required');
     }
     const filesPath = images?.map(file => `uploads/lands/${file.filename}`);
-    let locations = null;
-    let flag = false;
-    let newLocation = null;
-    const regex = /\[|\]/;
-    if (regex.test(createLandDto.locations.toString())) {
-      locations = createLandDto.locations;
-      flag = true;
-      newLocation = JSON.parse(locations);
-    } else {
-      locations = JSON.parse(`[${createLandDto.locations}]`);
-    }
+    // let locations = null;
+    // let flag = false;
+    // let newLocation = null;
+    // const regex = /\[|\]/;
+    // if (regex.test(createLandDto.locations.toString())) {
+    //   locations = createLandDto.locations;
+    //   flag = true;
+    //   newLocation = JSON.parse(locations);
+    // } else {
+    //   locations = JSON.parse(`[${createLandDto.locations}]`);
+    // }
 
     return this.landService.createLandCustom(areaId, {
       ...createLandDto,
       images: filesPath,
-      locations: flag === true ? newLocation : locations
+      //locations: flag === true ? newLocation : locations
+      locations:createLandDto.locations
     });
   }
   // @Post()
