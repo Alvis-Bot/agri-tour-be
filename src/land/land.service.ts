d63c8c38fe5599d084b7f6f02c0d623808cf3573
hoanghaiduong <hoanghaiduong1711@gmail.com>
2023-08-14 13:55:38 +0700

    commit

--- src/land/land.service.ts
+++ src/land/land.service.ts
@@ -1,18 +1,18 @@
-import {Inject, Injectable, NotFoundException} from "@nestjs/common";
-import {InjectRepository} from "@nestjs/typeorm";
-import {Repository} from "typeorm";
-import {Service} from "../common/enum/service";
-import {Land} from "../common/entities/land.entity";
-import {LandCreateDto} from "../common/dto/land-create.dto";
-import {ApiException} from "../exception/api.exception";
-import {IAreaService} from "../area/service/area";
-import {CategoryDetails} from "src/common/entities/category-detail.entity";
-import {ErrorMessages} from "../exception/error.code";
-import {StorageService} from "../storage/storage.service";
-import {ImageType} from "../common/enum";
-import {Transactional} from "typeorm-transactional";
-import {CategoryName} from "../common/enum/category";
-import {AreaService} from "../area/area.service";
+import { Inject, Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Service } from "../common/enum/service";
+import { Land } from "../common/entities/land.entity";
+import { LandCreateDto } from "../common/dto/land-create.dto";
+import { ApiException } from "../exception/api.exception";
+import { IAreaService } from "../area/service/area";
+import { CategoryDetails } from "src/common/entities/category-detail.entity";
+import { ErrorMessages } from "../exception/error.code";
+import { StorageService } from "../storage/storage.service";
+import { ImageType } from "../common/enum";
+import { Transactional } from "typeorm-transactional";
+import { CategoryName } from "../common/enum/category";
+import { AreaService } from "../area/area.service";
 
 @Injectable()
 export class LandService {
@@ -24,48 +24,48 @@ export class LandService {
   ) { }
 
 
-    async getLandByName(name: string): Promise<Land> {
-        const land = await this.landRepository.findOne({
-            where: {
-                name
-            }
-        })
-        if (!land) {
-            throw new ApiException(ErrorMessages.LAND_NOT_FOUND)
-        }
-        return land;
+  async getLandByName(name: string): Promise<Land> {
+    const land = await this.landRepository.findOne({
+      where: {
+        name
+      }
+    })
+    if (!land) {
+      throw new ApiException(ErrorMessages.LAND_NOT_FOUND)
+    }
+    return land;
+  }
+
+  @Transactional()
+  async createLand(areaId: string, dto: LandCreateDto, files: Express.Multer.File[]): Promise<any> {
+    const area = await this.areaService.getAreaById(areaId);
+    const land = await this.getLandByName(dto.name);
+
+    const productType = await this.getCategoryDetailById(dto.productTypeId, CategoryName.PRODUCT_NAME);
+    const soilType = await this.getCategoryDetailById(dto.soilTypeId, CategoryName.SOIL_NAME);
+    if (area && land) {
+      throw new ApiException(ErrorMessages.LAND_EXIST)
     }
 
-    @Transactional()
-    async createLand(areaId: string, dto: LandCreateDto, files: Express.Multer.File[]): Promise<any> {
-        const area = await this.areaService.getAreaById(areaId);
-        const land = await this.getLandByName(dto.name);
+    const imageName = await this.storageService.uploadMultiFiles(ImageType.CARD_LAND, files);
 
-        const productType = await this.getCategoryDetailById(dto.productTypeId, CategoryName.PRODUCT_NAME);
-        const soilType = await this.getCategoryDetailById(dto.soilTypeId, CategoryName.SOIL_NAME);
-        if (area && land) {
-            throw new ApiException(ErrorMessages.LAND_EXIST)
-        }
-
-        const imageName = await this.storageService.uploadMultiFiles(ImageType.CARD_LAND, files);
-
-        const creating = this.landRepository.create({
-            ...dto,
-            area,
-            soilType,
-            productType,
-            images: imageName
-        })
-        return await this.landRepository.save(creating);
+    const creating = this.landRepository.create({
+      ...dto,
+      area,
+      soilType,
+      productType,
+      images: imageName
+    })
+    return await this.landRepository.save(creating);
   }
 
 
-    async getCategoryDetailById(id: string, type: CategoryName): Promise<CategoryDetails> {
-        const categoryDetail = await this.categoryDetailRepository.createQueryBuilder('categoryDetail')
-            .where('categoryDetail.id = :id', {id})
-            .leftJoin('categoryDetail.category', 'category')
-            .where('category.name = :name', { name : type })
-            .getOne();
+  async getCategoryDetailById(id: string, type: CategoryName): Promise<CategoryDetails> {
+    const categoryDetail = await this.categoryDetailRepository.createQueryBuilder('categoryDetail')
+      .where('categoryDetail.id = :id', { id })
+      .leftJoin('categoryDetail.category', 'category')
+      .where('category.name = :name', { name: type })
+      .getOne();
     if (!categoryDetail) {
       throw new ApiException(ErrorMessages.CATEGORY_DETAIL_NOT_FOUND)
     }
@@ -80,25 +80,25 @@ export class LandService {
     // return mappedLands;
 
     return await this.landRepository
-        .createQueryBuilder('land')
-        .select([
-          'land.id',
-          'land.name',
-          'land.acreage',
-          'land.locations',
-          'land.images',
-          'area.id',
-          'area.name',
-          'soilType.id',
-          'soilType.name',
-          'productType.id',
-          'productType.name',
-          'productType.child_column',
-        ])
-        .leftJoin('land.area', 'area')
-        .leftJoin('land.productType', 'productType')
-        .leftJoin('land.soilType', 'soilType')
-        .getMany();
+      .createQueryBuilder('land')
+      .select([
+        'land.id',
+        'land.name',
+        'land.acreage',
+        'land.locations',
+        'land.images',
+        'area.id',
+        'area.name',
+        'soilType.id',
+        'soilType.name',
+        'productType.id',
+        'productType.name',
+        'productType.child_column',
+      ])
+      .leftJoin('land.area', 'area')
+      .leftJoin('land.productType', 'productType')
+      .leftJoin('land.soilType', 'soilType')
+      .getMany();
   }
 
   async getLandsByAreaId(areaId: string): Promise<Land[]> {
