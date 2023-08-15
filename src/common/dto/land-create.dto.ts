b59ef7b968e5deb705d930f1136845a3bef25c4b
hoanghaiduong <hoanghaiduong1711@gmail.com>
2023-08-15 23:35:42 +0700

    ss

--- src/common/dto/land-create.dto.ts
+++ src/common/dto/land-create.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Location as ILocation } from "../abc";
-import {IsNotEmpty, IsString} from "class-validator";
-import {Transform} from "class-transformer";
+import { IsNotEmpty, IsString } from "class-validator";
+import { Transform } from "class-transformer";
 
 
 export class LandCreateDto {
@@ -30,14 +30,28 @@ export class LandCreateDto {
   @IsNotEmpty()
   soilTypeId: string;
 
-  @IsNotEmpty()
+  // @IsNotEmpty()
+  // @ApiProperty({
+  //   example: '[{"latitude": 14.3322, "longitude": 14.3322 , "point": 1}]',
+  //   isArray: false,
+  // })
+  // @Transform(({ value }) => JSON.parse(value))
+  // locations: ILocation[];
   @ApiProperty({
-    example: '[{"latitude": 14.3322, "longitude": 14.3322 , "point": 1}]',
-    isArray: false,
+    type: 'array',
+    items: {
+      type: 'object',
+      properties: {
+        point: { type: 'number', example: 12 },
+        latitude: { type: 'number', example: 24.5 },
+        longitude: { type: 'number', example: 14.5 },
+      },
+    },
   })
-  @Transform(({ value }) => JSON.parse(value))
-  locations: ILocation[];
 
+  @IsNotEmpty()
+  @Transform(({ value }) => [JSON.parse(value)])
+  locations: ILocation[];
   @ApiProperty({ type: 'array', items: { type: 'string', format: 'binary' } })
   images: Express.Multer.File[];
 }
