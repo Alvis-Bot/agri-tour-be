31220fc30621511f2c18b8d11c031ae13ddd8897
hoanghaiduong <hoanghaiduong1711@gmail.com>
2023-08-22 11:27:37 +0700

    sss

--- src/storage/storage.service.ts
+++ src/storage/storage.service.ts
@@ -22,35 +22,23 @@ export class StorageService implements OnModuleInit {
   }
 
   private async uploadStorage(type: ImageType, file: Express.Multer.File): Promise<string> {
-    // switch (file.mimetype) {
-    //   case 'image/jpeg':
-    //   case 'image/png':
-    //     // Xử lý cho các loại hình ảnh JPEG và PNG
-    //     const imageName = await this.buildImageFileName(type, file?.fieldname);
-    //     const imagePath = await this.buildImageFilePath(type, imageName);
-    //     await sharp(file.buffer).toFile(imagePath);
-    //     //public\\uploads\\crops\\crops.Gg4axDvEYBr1.1692280106174.webp
-    //     // bỏ đi public và thay bằng /uploads
-    //     return imagePath.replace(/\\?public\\?uploads\\/, '\\uploads\\');
-    //   default:
-    //     // Xử lý cho các trường hợp mimetype không rơi vào các trường hợp trên
-    //     const fileName = await this.buildOtherFileName(type, file.filename)
-    //     const filePath = await this.buildImageFilePath(type, fileName);
-    //     await sharp(filePath).toFile(filePath)
-    //     return filePath.replace(/\\?public\\?uploads\\/, '\\uploads\\');
-    // }
-    const mimeType = file.mimetype;
-    const imageName = await this.buildImageFileName(type, file?.fieldname || file.filename);
-    const imagePath = await this.buildImageFilePath(type, imageName);
-
-    if (mimeType === 'image/jpeg' || mimeType === 'image/png') {
-      await sharp(file.buffer).toFile(imagePath);
-    } else {
-      await sharp(imagePath).toFile(imagePath);
+    switch (file.mimetype) {
+      case 'image/jpeg':
+      case 'image/png':
+        // Xử lý cho các loại hình ảnh JPEG và PNG
+        const imageName = await this.buildImageFileName(type, file?.filename || file.filename);
+        const imagePath = await this.buildImageFilePath(type, imageName);
+        await sharp(file.buffer).toFile(imagePath);
+        //public\\uploads\\crops\\crops.Gg4axDvEYBr1.1692280106174.webp
+        // bỏ đi public và thay bằng /uploads
+        return imagePath.replace(/\\?public\\?uploads\\/, '\\uploads\\');
+      default:
+        // Xử lý cho các trường hợp mimetype không rơi vào các trường hợp trên
+        const fileName = await this.buildOtherFileName(type, file.filename)
+        const filePath = await this.buildImageFilePath(type, fileName);
+        await sharp(filePath).toFile(filePath)
+        return filePath.replace(/\\?public\\?uploads\\/, '\\uploads\\');
     }
-
-    // Cập nhật đường dẫn dạng /uploads thay vì \\uploads
-    return imagePath.replace(/\\?public\\?uploads\\/, '/uploads/');
   }
 
   async uploadFile(type: ImageType, file: Express.Multer.File): Promise<string> {
