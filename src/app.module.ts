5b2924bc52868c3530cd4836f96c756340a15db1
hoanghaiduong <hoanghaiduong1711@gmail.com>
2023-12-04 17:57:33 +0700

    sss

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,21 @@
-import {Module} from '@nestjs/common';
-import {TypeOrmModule} from '@nestjs/typeorm';
-import {ConfigModule, ConfigService} from "@nestjs/config";
-import {UserModule} from './user/user.module';
-import {AuthModule} from './auth/auth.module';
-import {FarmModule} from './farm/farm.module';
-import {AreaModule} from './area/area.module';
-import {LandModule} from './land/land.module';
-import {AppController} from './app.controller';
-import {CategoriesModule} from './categories/categories.module';
-import {FarmingCalenderModule} from './farming_calender/farming_calender.module';
-import {CategoryDetailsModule} from './category-details/category-details.module';
-import {TypesModule} from './types/types.module';
-import {PersonsModule} from './providers/persons.module';
-import {validationSchema} from "./common/config/validation";
-import {addTransactionalDataSource} from "typeorm-transactional";
-import {DataSource} from "typeorm";
-import {StorageModule} from "./storage/storage.module";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { FarmModule } from './farm/farm.module';
+import { AreaModule } from './area/area.module';
+import { LandModule } from './land/land.module';
+import { AppController } from './app.controller';
+import { CategoriesModule } from './categories/categories.module';
+import { FarmingCalenderModule } from './farming_calender/farming_calender.module';
+import { CategoryDetailsModule } from './category-details/category-details.module';
+import { TypesModule } from './types/types.module';
+import { PersonsModule } from './providers/persons.module';
+import { validationSchema } from "./common/config/validation";
+import { addTransactionalDataSource } from "typeorm-transactional";
+import { DataSource } from "typeorm";
+import { StorageModule } from "./storage/storage.module";
 import { CropsModule } from './crops/crops.module';
 import { WorkOfDayModule } from './work-of-day/work-of-day.module';
 import { MaterialModule } from './material/material.module';
@@ -26,13 +26,14 @@ import { AgriculturalProductsModule } from './agricultural-products/agricultural
 import { CareScheduleModule } from './care-schedule/care-schedule.module';
 import { HarvestModule } from './harvest/harvest.module';
 import { StatisticsModule } from './statistics/statistics.module';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema,
-        envFilePath: [`.env`, `.env.${process.env.NODE_ENV}`], // load env
+      envFilePath: [`.env`, `.env.${process.env.NODE_ENV}`], // load env
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
@@ -79,7 +80,8 @@ import { StatisticsModule } from './statistics/statistics.module';
     CareScheduleModule,
     HarvestModule,
     StatisticsModule,
+    ContactModule,
   ],
-  controllers:[AppController]
+  controllers: [AppController]
 })
-export class AppModule {}
+export class AppModule { }
